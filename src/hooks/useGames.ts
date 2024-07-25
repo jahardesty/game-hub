62ce49131ca66038df2189a3dbc983917d05d7dd
jahardesty <81397442+jahardesty@users.noreply.github.com>
2024-07-24 20:04:29 -0400

    listing genres

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -22,6 +22,7 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     const controller = new AbortController();
@@ -31,12 +32,13 @@ const useGames = () => {
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
